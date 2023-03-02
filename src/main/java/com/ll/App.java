e0c7e14a20ecf07ed38e25ddc864d43516ceed30
yeonann <lkim82@naver.com>
2023-03-02 11:37:31 +0900

    step5

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,9 +1,12 @@
 package com.ll;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    private Scanner sc;
+    private final Scanner sc;
 
     public App(Scanner sc) {
         this.sc = sc;
@@ -13,23 +16,36 @@ public class App {
 
         System.out.println("== 명언 앱 ==");
 
+        long lastWiseSayingId = 0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
 
         while (true) {
+
             System.out.print("명령) ");
             String command = sc.nextLine().trim();
 
             if (command.equals("종료")) {
                 break;
             } else if (command.equals("등록")) {
+                long id = lastWiseSayingId + 1;
                 System.out.print("명언 : ");
-
                 String content = sc.nextLine().trim();
                 System.out.print("작가 : ");
-
                 String authorName = sc.nextLine().trim();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+                WiseSaying wiseSaying = new WiseSaying(id, content, authorName);
+                wiseSayings.add(wiseSaying);
+                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                lastWiseSayingId = id;
+            } else if (command.equals("목록")) {
+                System.out.printf("번호 / 작가 / 명언");
+                System.out.println("-".repeat(30));
 
+                for(int i = 0; i <wiseSayings.size(); i++) {
+                    WiseSaying wiseSaying = wiseSayings.get(i);
+
+                    System.out.printf("%d / %s / %s\n", wiseSaying.getId(), wiseSaying.getAuthorName(), wiseSaying.getContent());
+                }
 
             }
         }
