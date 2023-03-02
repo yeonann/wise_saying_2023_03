028c9204a1b1bad1f2ea3e9d3ca3c90db74d921d
yeonann <lkim82@naver.com>
2023-03-02 10:12:53 +0900

    step2

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,31 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+    private Scanner sc;
+    public App (Scanner sc) {
+        this.sc = sc;
+    }
+    public void run() {
+
+        System.out.println("== 명언 앱 ==");
+
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine().trim();
+
+            if (command.equals("종료")) {
+                break;
+            } else if (command.equals("등록")) {
+                System.out.print("명언 : ");
+
+                String content = sc.nextLine().trim();
+                System.out.print("작가 : ");
+
+                String authorName = sc.nextLine().trim();
+            }
+        }
+    }
+}
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,8 +1,13 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("== 명언 앱 ==");
-        System.out.println("명령) 종료");
+        Scanner sc = new Scanner(System.in);
+
+        new App(sc).run();
+
+        sc.close();
     }
 }
