08cb8ba9900168d96d2f4062c7e58e52fa09adcd
yeonann <lkim82@naver.com>
2023-03-02 09:36:11 +0900

    step1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,6 +2,7 @@ package com.ll;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("== 명언 앱 ==");
+        System.out.println("명령) 종료");
     }
 }
